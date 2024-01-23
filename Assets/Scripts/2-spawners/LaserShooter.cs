02f363df2bd1aa4a9dd3be1b587529241946e485
Hod_wys <92233601+hodwys@users.noreply.github.com>
2024-01-23 12:15:12 +0200

    2

---
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+/**
+ * This component spawns the given laser-prefab whenever the player clicks a given key.
+ * It also updates the "scoreText" field of the new laser.
+ */
+public class LaserShooter: ClickSpawner {
+    [SerializeField] NumberField scoreField;
+
+ 
+
+    protected override GameObject spawnObject() {
+
+
+        GameObject newObject = base.spawnObject();  // base = super
+
+        // Modify the text field of the new object.
+        if(newObject != null )
+        {
+            ScoreAdder newObjectScoreAdder = newObject.GetComponent<ScoreAdder>();
+            if (newObjectScoreAdder)
+                newObjectScoreAdder.SetScoreField(scoreField);
+
+
+            return newObject;
+        }
+        return null;
+      
+    }
+
+}
